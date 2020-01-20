fce2af023d82438ac0024283b3040f4dee530a00
austin benn <r.austinbenn@gmail.com>
2020-01-20 14:43:40 +0100

    s and q form bugs

--- bin/bet_animal.sh
+++ bin/bet_animal.sh
@@ -119,6 +119,8 @@ echo ${T1}
 ${ANTSPATH}/N4BiasFieldCorrection -d 3 -i ${T1}  -c [100x100x100x100,0.0000000001] -b [200] -o ${T1} --verbose 0
 ${ANTSPATH}/DenoiseImage -d 3 -i ${T1} -o sanlm_${T1}
 ${ANTSPATH}/ImageMath  3 sanlm_${T1} TruncateImageIntensity sanlm_${T1} 0.05 0.999 
+#### ants has a tendency to remove the sform which we need for fsl registerting. 
+${FSLDIR}/fslorient -copyqform2sform sanlm_${T1}
 
 if [[  -f ${premat}  ]] ;then
 
